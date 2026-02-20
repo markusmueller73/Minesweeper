50ff49c64013f23141f2d82e6df2a1b925e54155
Markus Müller <33749850+markusmueller73@users.noreply.github.com>
2026-02-20 14:36:35 +0000

    Delete src directory

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-mod game;
-mod minesweeper;
-mod ui;
-
-fn main() {
-    crate::game::run();
-}
--- src/ui.rs
+++
@@ -1,7 +0,0 @@
-mod button;
-pub mod main_menu;
-pub mod playfield;
-mod rectangle;
-
-pub const MENU_BUTTON_WIDTH: f32 = 256.0;
-pub const MENU_BUTTON_HEIGHT: f32 = 65.0;
