71676a3a8c19d9acc9e53e597d0934535ae98d92
Markus Müller <33749850+markusmueller73@users.noreply.github.com>
2026-02-24 13:28:13 +0000

    Delete src directory

--- src/engine.rs
+++
@@ -1,5 +0,0 @@
-pub mod assets;
-pub mod input;
-pub mod render;
-pub mod ui;
-pub mod update;
--- src/game/constants.rs
+++
@@ -1,9 +0,0 @@
-
-pub const APP_NAME: &str = "Minesweeper";
-
-pub const DEFAULT_WINDOW_WIDTH: u32 = 450;
-pub const DEFAULT_WINDOW_HEIGHT: u32 = 500;
-
-pub const CELL_SIZE_SMALL_BOARD: f32 = 40.0;
-pub const CELL_SIZE_MED_BOARD: f32 = 35.0;
-pub const CELL_SIZE_LARGE_BOARD: f32 = 25.0;
